5623fde4cac00eb00730cca1f4d49e9e0c019ea7
wom987 <45610462+wom987@users.noreply.github.com>
2020-11-20 21:21:40 -0600

    Agregando geolocalizacion

--- ProyectoDWA2.0/js/app.js
+++ ProyectoDWA2.0/js/app.js
@@ -1,3 +1,4 @@
+locateMe();
 function comprar(value) {
   console.log(value);
   Swal.fire({
@@ -59,3 +60,48 @@ document.addEventListener("DOMContentLoaded", function () {
     direction: "top",
   });
 });
+function locateMe() {
+  //inicializador de funciones
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation) {
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+  //configuracion de coordenadas para mostrar el mapa
+  function success(position) {
+    //obtener valores de datos
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    //direcitons de dato del negocio
+    var miLugar = new google.maps.LatLng(13.705261, -89.21054);
+    var coord = { lat: latitude, lng: longitude };
+    var map = new google.maps.Map(document.getElementById("custom-places"), {
+      zoom: 10,
+      center: coord,
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map,
+      // seteado de labes del mapa
+      title: "Tu Ubicacion",
+      label: "Aca Estas",
+    });
+    //
+    var marker1 = new google.maps.Marker({
+      position: miLugar,
+      map: map,
+      // seteado de labes del mapa
+      title: "IoT Department",
+      label: "Nosotros",
+    });
+  }
+  // errores
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
